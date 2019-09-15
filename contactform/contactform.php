be2e8e4488c2f489a5a7289b593429e1d15cb151
Shane ONeill <oneill.shanepatrick@gmail.com>
2019-09-14 22:01:52 -0400

    photo grid updates

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -8,7 +8,7 @@
 /***************** Configuration *****************/
 
   // Replace with your real receiving email address
-  $contact_email_to = "contact@example.com";
+  $contact_email_to = "rebecca.kostell@gmail.com";
 
   // Title prefixes
   $subject_title = "Contat Form Message:";
